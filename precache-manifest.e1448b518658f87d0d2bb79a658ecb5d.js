a3890d51045071d2c3865143bc4c55b743b4860a
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-02-05 12:45:21 +0100

    Updates

---
+++ precache-manifest.e1448b518658f87d0d2bb79a658ecb5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8079549f187f800fb9c54ad678b28b",
+    "url": "/fbw9todo/index.html"
+  },
+  {
+    "revision": "27bc950e3f6c781d7817",
+    "url": "/fbw9todo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "245ae24ff48b66aad4fa",
+    "url": "/fbw9todo/static/css/main.cdac3187.chunk.css"
+  },
+  {
+    "revision": "27bc950e3f6c781d7817",
+    "url": "/fbw9todo/static/js/2.0bf3e7bc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/fbw9todo/static/js/2.0bf3e7bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "245ae24ff48b66aad4fa",
+    "url": "/fbw9todo/static/js/main.8299d062.chunk.js"
+  },
+  {
+    "revision": "a2a447ad3681acf3d64d",
+    "url": "/fbw9todo/static/js/runtime-main.5d220b74.js"
+  }
+]);
